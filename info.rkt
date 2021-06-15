3bd3625436ea5f8e6e4d6e7e24dd5d0c0ed971fa
Bogdan Popa <bogdan@defn.io>
2021-06-15 11:23:22 +0300

    restore backwards-compatibility for expressions
    Closes #4

--- info.rkt
+++ info.rkt
@@ -2,9 +2,9 @@
 (define collection "struct-define")
 (define deps '("base"))
 (define build-deps '("racket-doc"
+                     "rackunit-lib"
                      "sandbox-lib"
-                     "scribble-lib"
-                     ))
+                     "scribble-lib"))
 (define version "0.1")
 (define pkg-authors '(jeapostrophe))
 (define scribblings '(("struct-define.scrbl" () ("Syntax Extensions"))))
